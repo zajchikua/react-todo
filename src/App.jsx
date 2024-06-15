75cecc76bbabd07a93b218b9b521f8d5fa806188
anastasiya <miroshnychenkoa@yahoo.com>
2024-06-15 14:20:47 -0700

    Lesson1_2-Working-and Handling error

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,6 @@ import "./App.css"
 import TodoList from "./TodoList.jsx"
 import AddTodoForm from "./AddTodoForm.jsx"
 
-const todoList = [
-	{
-		id: "001",
-		title: "Complete lesson 1 for CTD",
-		url: "https://classes.codethedream.org/course/react-v2/hawk?week=2",
-		author: "Nastya M.",
-		num_comments: 0,
-		points: 5,
-	},
-	{
-		id: "002",
-		title: "Setup printer for the MAC",
-		url: "https://classes.codethedream.org/course/react-v2/hawk?week=1",
-        author: "Nastya & Mom",
-        num_comments: 1,
-        points:2,
-	},
-	{
-		id: "003",
-		title: "Collect documents for the passport",
-		url: "https://classes.codethedream.org/course/react-v2/hawk?week=3",
-        author: "Nastya & Dad",
-        num_comments: 2,
-        points:2,
-	},
-];
-
 function App() {
   const [count, setCount] = useState(0)
 
@@ -40,6 +13,7 @@ function App() {
         <h1>Todo list</h1>
         <TodoList />
         <hr />
+        <h1>Add Todo Form</h1>
         <AddTodoForm />
     </div>
   ) ;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,26 +1,52 @@
 import * as React from 'react';
 
+const todoList = [
+	{
+		id: "001",
+		title: "Complete lesson 1 for CTD",
+		url: "https://classes.codethedream.org/course/react-v2/hawk?week=2",
+		author: "Nastya M.",
+		num_comments: 0,
+		points: 5,
+	},
+	{
+		id: "002",
+		title: "Setup printer for the MAC",
+		url: "https://classes.codethedream.org/course/react-v2/hawk?week=1",
+        author: "Nastya & Mom",
+        num_comments: 1,
+        points:2,
+	},
+	{
+		id: "003",
+		title: "Collect documents for the passport",
+		url: "https://classes.codethedream.org/course/react-v2/hawk?week=3",
+        author: "Nastya & Dad",
+        num_comments: 2,
+        points:2,
+	},
+];
+
 function TodoList() {
     return (
 
         <div>
-            <h1>Todo list</h1>
     	    <ul>
-    		    {list.map(function (todoList){
-    		    return
-    		    <li key={todoList.id}>
-    		        <span>
-    		            <a href={todoList.url}>{todoList.title}</a>
-    		        </span>
-    		        <span>{todoList.author}</span>
-    		        <span>{todoList.num_comments}</span>
-                    <span>{todoList.points}</span>
-    		    </li>;
+    		    {todoList.map(function (todoList){
+    		        return (
+    		            <li key={todoList.id}>
+    		                <span>
+    		                    <a href={todoList.url}>{todoList.title}</a>
+    		                </span>
+    		                <span>{todoList.author}</span>
+    		                <span>{todoList.num_comments}</span>
+                            <span>{todoList.points}</span>
+    		            </li>
+    		        );
     		    })}
     	    </ul>
         </div>
-      ) ;
-
+    );
 }
 
 export default TodoList;
