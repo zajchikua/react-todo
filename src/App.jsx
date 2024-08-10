dbe152a14ccc4961845d6fb50f4f7e6b936df9cc
anastasiya <miroshnychenkoa@yahoo.com>
2024-08-10 16:45:32 -0700

    Lesson1_7 - adding async storage to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,48 @@ import viteLogo from "/vite.svg"
 import "./App.css"
 import TodoList from './TodoList.jsx';
 import AddTodoForm from './AddTodoForm.jsx';
-import React from "react"
+import React from "react";
 
-function useSemiPersistentState() {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem('savedTodoList')) || []
-  );
+function App() {
+ const existingTodo = JSON.parse(localStorage.getItem('savedTodoList')) ?? [];
+ const [todoList, setTodoList] = React.useState([]);
+
+    React.useEffect(() => {
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const object = {
+                    data: {
+                        todoList: existingTodo,
+                    },
+                };
+                    resolve(object);
+            }, 2000);
+        }).then((result) => {
+            setTodoList(result.data.todoList)
+        });
+    }, []);
 
  React.useEffect(() => {
         localStorage.setItem('savedTodoList',JSON.stringify(todoList));
-    }, [todoList, setTodoList]);
-
-    return [todoList, setTodoList];
-}
-
-function App() {
-
-  const [todoList, setTodoList] = useSemiPersistentState();
+    }, [todoList]);
 
     function addTodo(newTodo){
         setTodoList((prevTodoList) => [...prevTodoList, newTodo]);
    }
 
-  return (
-    <div>
-        <h1>Todo list</h1>
-        <h1>Add Todo Form</h1>
-        <AddTodoForm onAddTodo={addTodo} />
-        <TodoList todoList={todoList} />
-    </div>
-  );
-}
+    function removeTodo(id){
+           const filteredTodos = todoList.filter((todo) => todo.id !== id);
+           setTodoList(filteredTodos);
+      }
+
+     return (
+       <div>
+           <h1  style={{ color: "magenta" }}>Todo list</h1>
+           <h1  style={{ color: "green" }}>Add Todo Form</h1>
+           <AddTodoForm onAddTodo={addTodo} />
+           <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+       </div>
+     );
+   }
 
 export default App;
